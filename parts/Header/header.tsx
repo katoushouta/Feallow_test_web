9770b7b0074e738a806dcd7d1020fd42b032c76d
katoushouta <shouta.katou@link-u.co.jp>
2022-02-10 21:20:57 +0900

    ヘッダーposition: fixedやめ

--- parts/Header/header.tsx
+++ parts/Header/header.tsx
@@ -1,8 +1,8 @@
 import s from "./header.module.sass"
 
-export const Header: React.FC<{isTop: boolean}> = ({isTop}) => {
+export const Header: React.FC<{}> = () => {
     return (
-        <div className={`${s.header} ${isTop&& s.top}`}>
+        <div className={`${s.header}`}>
             <div className={s.container}>
                 <div className={s.logo}>
                     <img src={"https://placehold.jp/320x96.png?text=LOGO"} />
